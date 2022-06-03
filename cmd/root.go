3aed38a5165e783dac667905fb9653e96e42cb88
alexhokl <alexhokl@gmail.com>
2022-06-03 21:09:05 +0800

    Adapted to use github.com/alexhokl/helper/cli

--- cmd/root.go
+++ cmd/root.go
@@ -16,11 +16,8 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-	"os"
+	"github.com/alexhokl/helper/cli"
 	"github.com/spf13/cobra"
-
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -62,24 +59,5 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".strava-cli" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".strava-cli")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
+	cli.ConfigureViper(cfgFile, "strava-cli", true, "")
 }
